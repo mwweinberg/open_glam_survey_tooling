7cc2a53ef7da179bbf0f9ef95e0e0ba878a278d7
Michael Weinberg <mwweinberg@users.noreply.github.com>
2025-07-16 10:26:27 -0400

    cut out some columns and fixed pretty_url

--- contentful2csv.py
+++ contentful2csv.py
@@ -59,13 +59,10 @@ def build_institution_list():
 
 build_institution_list()
 
-# #just for testing, can be deleted
-# for i in list_of_institutions_with_fields:
-#     print(i)
-#     print('****')
+
 
 #now you have a list of all of the institutions with their data platforms 
-#go through each data platform to see if there is new data
+#and add them
 for i in list_of_institutions_with_fields:
     #this will create a list of just the platforms (may be a list of one)
     this_institutions_data_platforms = i['data_platform']
@@ -91,25 +88,31 @@ for i in list_of_institutions_with_fields:
         platform_counter += 1
         #print(platform_count_dict)
         #print('^^^^^^^')
-print('updated dictionaries')
+print('added data platforms')
 
-    #print('*****')
+#the 'pretty_url' field is just the stub. This step adds the full url
 
-# just for testing, can be deleted
-# for i in list_of_institutions_with_fields:
-#     print(i)
-#     print('*******')
+for i in list_of_institutions_with_fields:
+    updated_pretty_url = 'https://survey.glamelab.org/institutions/'+ i['pretty_url']
+    i['pretty_url'] = updated_pretty_url
+
+
 
 
 
 #every key in the dict must be in this list, but having a key in this list without a corresponding value in an item is not a problem
 #that's why you have a bunch of 'open_data_volume_XY' entries at the end
-fieldnames = ['institution_name', 'institution_name_english', 'pretty_url', 'country', 'part_of', 'institution_type', 'institution_website', 'institution_wikidata', 'institution_description', 'first_open_access_instance', 'first_open_access_instance_citation', 'open_access_scope', 'open_access_policy', 'tk_tce_policy', 'rights_statement_compliance', 'rights_statement_metadata', 'rights_statement_metadata_url', 'admission_policy', 'institution_api', 'institution_github', 'data_platform', 'institution_contentful_id','open_data_volume_name0', 'open_data_volume_number0', 'open_data_volume_url0','open_data_volume_name1', 'open_data_volume_number1', 'open_data_volume_url1','open_data_volume_name2', 'open_data_volume_number2', 'open_data_volume_url2', 'open_data_volume_name3', 'open_data_volume_number3', 'open_data_volume_url3', 'open_data_volume_name4', 'open_data_volume_number4', 'open_data_volume_url4', 'open_data_volume_name5', 'open_data_volume_number5', 'open_data_volume_url5']
+fieldnames = ['institution_name', 'institution_name_english', 'pretty_url', 'country', 'part_of', 'institution_type', 'institution_website', 'institution_wikidata', 'first_open_access_instance', 'first_open_access_instance_citation', 'open_access_scope', 'open_access_policy', 'tk_tce_policy', 'rights_statement_compliance', 'rights_statement_metadata', 'rights_statement_metadata_url', 'admission_policy', 'institution_api', 'institution_github',  'open_data_volume_name0', 'open_data_volume_number0', 'open_data_volume_url0','open_data_volume_name1', 'open_data_volume_number1', 'open_data_volume_url1','open_data_volume_name2', 'open_data_volume_number2', 'open_data_volume_url2', 'open_data_volume_name3', 'open_data_volume_number3', 'open_data_volume_url3', 'open_data_volume_name4', 'open_data_volume_number4', 'open_data_volume_url4', 'open_data_volume_name5', 'open_data_volume_number5', 'open_data_volume_url5']
+
+#these are the fieldnames that exist in the dictionary but will be skipped.
+#this works because the instantiation of DictWriter below uses the `extrasaction='ignore'` argument, which means that when it finds a key not in the fieldnames list it just skips it (default behavior is to stop and raise an issue)
+#you are just keeping them in case you need to restore them in the future for some reason
+#['institution_contentful_id','data_platform','institution_description',]
 
 #name of output file 
 with open('contentful2csv.csv', 'w') as csv_file:
     #imports the fieldnames from above
-    writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+    writer = csv.DictWriter(csv_file, fieldnames=fieldnames, extrasaction='ignore')
     #writes the header row based on the fieldnames 
     writer.writeheader()
     #writes one row per institution blob
