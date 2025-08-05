613fa2bc4d085f949f2af7346a191870507b086d
Michael Weinberg <mwweinberg@users.noreply.github.com>
2025-08-05 09:02:31 -0400

    added pagination handling

--- contentful2csv.py
+++ contentful2csv.py
@@ -22,17 +22,42 @@ def build_institution_list():
 
     #STEP 1: get all of the 'surveyInstitution' entries 
     #returns some sort of object of the entries
-    entries_just_institutions = client.entries(space_ID, api_environment_id).all({'content_type': 'surveyInstitution'})
-    #append all of the object ids to institution_id_list
-    for i in entries_just_institutions:
-        #extract the institution ID
-        entry_id = i.id
-        #add the institution id to the list
-        institution_id_list.append(entry_id)
-        #print for testing/debugging 
-        # print(entry_id)
+    # entries_just_institutions = client.entries(space_ID, api_environment_id).all({'content_type': 'surveyInstitution'})
+    # #append all of the object ids to institution_id_list
+    # for i in entries_just_institutions:
+    #     #extract the institution ID
+    #     entry_id = i.id
+    #     #add the institution id to the list
+    #     institution_id_list.append(entry_id)
+    #     #print for testing/debugging 
+    #     # print(entry_id)
+    #     #print(f'institution_id_list length = {len(institution_id_list)}')
+
+    #updated version of STEP 1 to deal with contentful pagination
+    limit = 100
+    skip = 0
+
+    while True:
+        entries = client.entries(space_ID, api_environment_id).all({
+            'content_type': 'surveyInstitution',
+            'limit': limit,
+            'skip': skip
+        })
+
+        if not entries:
+            break
+
+        for entry in entries:  # entries is iterable directly
+            institution_id_list.append(entry.sys['id'])  # or entry.id
+
+        if len(entries) < limit:
+            break  # no more pages
+        
+        print(f'downloaded first {skip + 100} institutions')
+        skip += limit
     
     #STEP 2: add institutions (with fields) to list_of_institution_with_fields
+    getting_institution_data_counter = 1
     for i in institution_id_list:
         #get the entry
         entry = client.entries(space_ID, api_environment_id).find(i)
@@ -42,17 +67,22 @@ def build_institution_list():
         fields['institution_contentful_id'] = i
         #append to list_of_institutions_with_fields
         list_of_institutions_with_fields.append(fields)
+        print(f'getting data for institution number {getting_institution_data_counter}')
+        getting_institution_data_counter += 1
 
     #STEP 3 update list_of_institutions_with_fields to include uid for data platform entries 
     for i in list_of_institutions_with_fields:
         #this is going to be how you extract the list of data platform references 
         #create a temporary list to hold all of the ids
         list_of_data_platforms = []
-        for e in i['data_platform']:
-            #add each platform id to the temporary list
-            list_of_data_platforms.append(e.id)
-        #replace existing data_platform entry with the list because that will be easier to work with
-        i['data_platform'] = list_of_data_platforms
+        try:
+            for e in i['data_platform']:
+                #add each platform id to the temporary list
+                list_of_data_platforms.append(e.id)
+            #replace existing data_platform entry with the list because that will be easier to work with
+            i['data_platform'] = list_of_data_platforms
+        except:
+            print(f"(line 58) no platform for {i}")
         # print(list_of_data_platforms)
     print("completed build_institution_list")
 
@@ -63,31 +93,37 @@ build_institution_list()
 
 #now you have a list of all of the institutions with their data platforms 
 #and add them
+getting_platforms_data_counter = 1
 for i in list_of_institutions_with_fields:
-    #this will create a list of just the platforms (may be a list of one)
-    this_institutions_data_platforms = i['data_platform']
-    #counter to keep track of how many platforms 
-    platform_counter = 0
-    #iterate through it to get the info about the individual data platforms
-    for e in this_institutions_data_platforms:
-        #get the entry 
-        entry = client.entries(space_ID, api_environment_id).find(e)
-        #get the fields in the entry 
-        fields = entry.fields()
-        #print(fields)
-        #print('&&&&&&')
-        #constructs the open_data_volume_name and _number so that it can iterate
-        open_data_volume_name = 'open_data_volume_name'+str(platform_counter)
-        open_data_volume_number = 'open_data_volume_number'+str(platform_counter)
-        open_data_volume_url = 'open_data_volume_url'+str(platform_counter)
-        #creates a dictionary to add to the i entry 
-        platform_count_dict = {open_data_volume_name: fields['open_data_platform'], open_data_volume_number: fields['open_data_volume'], open_data_volume_url: fields['open_data_platform_url']}
-        #puts the whole platform_count_dict at the end of the existing i entry 
-        i.update(platform_count_dict)
-        #iterates the platform_counter
-        platform_counter += 1
-        #print(platform_count_dict)
-        #print('^^^^^^^')
+    try:
+        #this will create a list of just the platforms (may be a list of one)
+        this_institutions_data_platforms = i['data_platform']
+        #counter to keep track of how many platforms 
+        platform_counter = 0
+        #iterate through it to get the info about the individual data platforms       
+        for e in this_institutions_data_platforms:
+            #get the entry 
+            entry = client.entries(space_ID, api_environment_id).find(e)
+            #get the fields in the entry 
+            fields = entry.fields()
+            #print(fields)
+            #print('&&&&&&')
+            #constructs the open_data_volume_name and _number so that it can iterate
+            open_data_volume_name = 'open_data_volume_name'+str(platform_counter)
+            open_data_volume_number = 'open_data_volume_number'+str(platform_counter)
+            open_data_volume_url = 'open_data_volume_url'+str(platform_counter)
+            #creates a dictionary to add to the i entry 
+            platform_count_dict = {open_data_volume_name: fields['open_data_platform'], open_data_volume_number: fields['open_data_volume'], open_data_volume_url: fields['open_data_platform_url']}
+            #puts the whole platform_count_dict at the end of the existing i entry 
+            i.update(platform_count_dict)
+            #iterates the platform_counter
+            platform_counter += 1
+            #print(platform_count_dict)
+            #print('^^^^^^^')
+            print(f'getting data platforms for institution number {getting_platforms_data_counter}')
+            getting_platforms_data_counter += 1
+    except:
+        print(f"(line 96) no data platform for {i}")
 print('added data platforms')
 
 
@@ -98,7 +134,7 @@ for i in list_of_institutions_with_fields:
     i['pretty_url'] = updated_pretty_url
 
     #if the first_open_access_instance date is empty it defaults to 1/1/70. That is read as "null" when the site is being built. This changes the output to match that. 
-    if i['first_open_access_instance'] == '1970-01-01T00:00:00.000Z':
+    if i.get('first_open_access_instance') == '1970-01-01T00:00:00.000Z':
         i['first_open_access_instance'] = 'null'
 
 
